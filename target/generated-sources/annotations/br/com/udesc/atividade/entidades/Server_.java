a627b04f625b37298b3568487bd33f5b23a1ae5d
Robson Sestrem <sestrem.robson@gmail.com>
2022-02-12 17:18:05 -0300

    ajustes git

--- target/generated-sources/annotations/br/com/udesc/atividade/entidades/Server_.java
+++ target/generated-sources/annotations/br/com/udesc/atividade/entidades/Server_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-04T00:30:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-05T14:07:42")
 @StaticMetamodel(Server.class)
 public class Server_ { 
 
--- target/generated-sources/annotations/br/com/udesc/atividade/entidades/SolicitacaoServico_.java
+++ target/generated-sources/annotations/br/com/udesc/atividade/entidades/SolicitacaoServico_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-04T00:30:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-05T14:07:42")
 @StaticMetamodel(SolicitacaoServico.class)
 public class SolicitacaoServico_ { 
 
